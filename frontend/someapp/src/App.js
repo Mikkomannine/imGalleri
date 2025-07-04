796160c69378bbc22b7f3b7df334a1030871e7c3
Mikko <mikko.manninen3@metropolia.fi>
2025-07-04 20:54:21 +0900

    1.3.3

--- frontend/someapp/src/App.js
+++ frontend/someapp/src/App.js
@@ -1,64 +1,21 @@
-import MyProfile from './pages/myProfile';
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
-import PropTypes from 'prop-types';
 
+// Pages
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import PostForm from './pages/postform';
-import ProfilePage from './pages/profile';
-import Home from './pages/home';
-import Post from './pages/post';
-import EditProfilePage from './pages/EditProfilePage';
-import Layout from './components/layout';
 import ForgotPassword from './pages/forgotPassword';
 import ResetPassword from './pages/ResetPassword';
+import Home from './pages/home';
+import MyProfile from './pages/myProfile';
+import PostForm from './pages/postform';
+import Post from './pages/post';
+import ProfilePage from './pages/profile';
+import EditProfilePage from './pages/EditProfilePage';
 import Following from './pages/Following';
 
-
-function RoutesComponent({ setIsAuthenticated, isAuthenticated }) {
-  const location = useLocation();
-  const showLayout = !['/login', '/signup'].includes(location.pathname);
-
-  const routes = [
-    { path: "/", element: <Home /> },
-    { path: "/myprofile", element: <MyProfile /> },
-    { path: "/postform", element: <PostForm /> },
-    { path: "/post/:id", element: <Post /> },
-    { path: "/profile/:id", element: <ProfilePage /> },
-    { path: "/edit/profile/:id", element: <EditProfilePage /> },
-    { path: "/login", element: <Login setIsAuthenticated={setIsAuthenticated} />, isAuthenticated: false },
-    { path: "/signup", element: <Signup setIsAuthenticated={setIsAuthenticated} />, isAuthenticated: false },
-    { path: "/forgot-password", element: <ForgotPassword />, isAuthenticated: false },
-    { path: "/reset-password/:token", element: <ResetPassword />, isAuthenticated: false },
-    { path: "/following", element: <Following/> },
-
-  ];
-
-  return (
-    <>
-      {showLayout && <Layout />}
-      <Routes>
-        {routes.map(({ path, element, isAuthenticated: routeIsAuthenticated }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              isAuthenticated || routeIsAuthenticated === false
-                ? element
-                : <Navigate to="/login" />
-            }
-          />
-        ))}
-      </Routes>
-    </>
-  );
-}
-
-RoutesComponent.propTypes = {
-  setIsAuthenticated: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool.isRequired,
-};
+// Layout
+import Layout from './components/layout';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(Boolean(localStorage.getItem("token")));
@@ -66,7 +23,29 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <RoutesComponent setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />
+        <Routes>
+
+          {/* Public routes */}
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/signup" element={<Signup setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+          {/* Protected routes wrapped in Layout */}
+          <Route element={isAuthenticated ? <Layout /> : <Navigate to="/login" />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/myprofile" element={<MyProfile />} />
+            <Route path="/postform" element={<PostForm />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/profile/:id" element={<ProfilePage />} />
+            <Route path="/edit/profile/:id" element={<EditProfilePage />} />
+            <Route path="/following" element={<Following />} />
+          </Route>
+
+          {/* Catch-all for invalid paths */}
+          <Route path="*" element={<Navigate to="/" />} />
+
+        </Routes>
       </BrowserRouter>
     </div>
   );
